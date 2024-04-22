02485015891dfdde08701953deda0a35ff423e8a
ImxYJL <allensain14@gmail.com>
2024-04-22 12:05:02 +0900

    fix: 충돌 해결 과정에서 생긴 문제 해결

--- src/components/Form/ExpirationDateForm.tsx
+++ src/components/Form/ExpirationDateForm.tsx
@@ -33,6 +33,10 @@ const ExpirationDateForm = ({
 
   const updateInputValidity = (index: string, isValid: boolean) => {
     setInputValidities((prevValidities) => ({
+      ...prevValidities,
+      [index]: isValid,
+    }));
+  };
 
   useEffect(() => {
     const allValid = Object.values(inputValidities).every((isValid) => isValid);
